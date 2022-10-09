08c85b0002d1735ac7f16df0e9dff1bd894a870f
Michael Vescovo <m.vescovo@gmail.com>
2022-10-09 20:53:23 +1100

    Initial Commit

---
+++ test_ios_workflow/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test_ios_workflow
+//
+//  Created by Michael Vescovo on 9/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test_ios_workflow/test_ios_workflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_ios_workflowApp.swift
+//  test_ios_workflow
+//
+//  Created by Michael Vescovo on 9/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct test_ios_workflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
